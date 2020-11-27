02f0a77ec9c47c587030f0cdd8a9fbe452863dac
zoet24 <zt12542@my.bristol.ac.uk>
2020-11-27 18:26:09 +0000

    Bug fix 12?

--- assets/js/about.js
+++ assets/js/about.js
@@ -25,71 +25,71 @@ $(window).scroll(function() {
        wS = $(this).scrollTop();
 
    if ((wS > (hTa + (hHa/2) - ((1.05*wH)/2))) && (wS < (hTa + (hHa/2) - ((0.95*wH)/2)))){
-       $("#about-text-1").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-1", "about.css");
+    //    $("#about-text-1").addClass("highlight", "about.css");
+       $("#about-text-1").addClass("img-1", "about.css");
    }
    else if ((wS > (hTb + (hHb/2) - ((1.05*wH)/2))) && (wS < (hTb + (hHb/2) - ((0.95*wH)/2)))){
-       $("#about-text-2").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-2", "about.css");
+    //    $("#about-text-2").addClass("highlight", "about.css");
+       $("#about-text-2").addClass("img-2", "about.css");
    }
    else if ((wS > (hTc + (hHc/2) - ((1.05*wH)/2))) && (wS < (hTc + (hHc/2) - ((0.95*wH)/2)))){
-       $("#about-text-3").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-3", "about.css");
+    //    $("#about-text-3").addClass("highlight", "about.css");
+       $("#about-text-3").addClass("img-3", "about.css");
    }
    else if ((wS > (hTd + (hHd/2) - ((1.05*wH)/2))) && (wS < (hTd + (hHd/2) - ((0.95*wH)/2)))){
-       $("#about-text-4").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-4", "about.css");
+    //    $("#about-text-4").addClass("highlight", "about.css");
+       $("#about-text-4").addClass("img-4", "about.css");
    }
    else if ((wS > (hTe + (hHe/2) - ((1.05*wH)/2))) && (wS < (hTe + (hHe/2) - ((0.95*wH)/2)))){
-       $("#about-text-5").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-5", "about.css");
+    //    $("#about-text-5").addClass("highlight", "about.css");
+       $("#about-text-5").addClass("img-5", "about.css");
    }
-   else if ((wS > (hTf + (hHf/2) - ((1.05*wH)/2))) && (wS < (hTf + (hHf/2) - ((0.95*wH)/2)))){
-       $("#about-text-6").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-6", "about.css");
-   }
-   else if ((wS > (hTg + (hHg/2) - ((1.05*wH)/2))) && (wS < (hTg + (hHg/2) - ((0.95*wH)/2)))){
-       $("#about-text-7").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-7", "about.css");
-   }
-   else if ((wS > (hTh + (hHh/2) - ((1.05*wH)/2))) && (wS < (hTh + (hHh/2) - ((0.95*wH)/2)))){
-       $("#about-text-8").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-8", "about.css");
-   }
-   else if ((wS > (hTi + (hHi/2) - ((1.05*wH)/2))) && (wS < (hTi + (hHi/2) - ((0.95*wH)/2)))){
-       $("#about-text-9").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-9", "about.css");
-   }
-   else if ((wS > (hTj + (hHj/2) - ((1.05*wH)/2))) && (wS < (hTj + (hHj/2) - ((0.95*wH)/2)))){
-       $("#about-text-10").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-10", "about.css");
-   }
-   else if ((wS > (hTk + (hHk/2) - ((0.95*wH)/1))) && (wS < (hTk + (hHk/2) - ((0.8*wH)/1)))){
-       $("#about-text-11").addClass("highlight", "about.css");
-       $("main").addClass("about-bg-img-11", "about.css");
-   }
+//    else if ((wS > (hTf + (hHf/2) - ((1.05*wH)/2))) && (wS < (hTf + (hHf/2) - ((0.95*wH)/2)))){
+//        $("#about-text-6").addClass("highlight", "about.css");
+//        $("main").addClass("img-6", "about.css");
+//    }
+//    else if ((wS > (hTg + (hHg/2) - ((1.05*wH)/2))) && (wS < (hTg + (hHg/2) - ((0.95*wH)/2)))){
+//        $("#about-text-7").addClass("highlight", "about.css");
+//        $("main").addClass("about-bg-img-7", "about.css");
+//    }
+//    else if ((wS > (hTh + (hHh/2) - ((1.05*wH)/2))) && (wS < (hTh + (hHh/2) - ((0.95*wH)/2)))){
+//        $("#about-text-8").addClass("highlight", "about.css");
+//        $("main").addClass("about-bg-img-8", "about.css");
+//    }
+//    else if ((wS > (hTi + (hHi/2) - ((1.05*wH)/2))) && (wS < (hTi + (hHi/2) - ((0.95*wH)/2)))){
+//        $("#about-text-9").addClass("highlight", "about.css");
+//        $("main").addClass("about-bg-img-9", "about.css");
+//    }
+//    else if ((wS > (hTj + (hHj/2) - ((1.05*wH)/2))) && (wS < (hTj + (hHj/2) - ((0.95*wH)/2)))){
+//        $("#about-text-10").addClass("highlight", "about.css");
+//        $("main").addClass("about-bg-img-10", "about.css");
+//    }
+//    else if ((wS > (hTk + (hHk/2) - ((0.95*wH)/1))) && (wS < (hTk + (hHk/2) - ((0.8*wH)/1)))){
+//        $("#about-text-11").addClass("highlight", "about.css");
+//        $("main").addClass("about-bg-img-11", "about.css");
+//    }
    else {
-       $("#about-text-1").removeClass("highlight", "about.css");
-       $("#about-text-2").removeClass("highlight", "about.css");
-       $("#about-text-3").removeClass("highlight", "about.css");
-       $("#about-text-4").removeClass("highlight", "about.css");
-       $("#about-text-5").removeClass("highlight", "about.css");
-       $("#about-text-6").removeClass("highlight", "about.css");
-       $("#about-text-7").removeClass("highlight", "about.css");
-       $("#about-text-8").removeClass("highlight", "about.css");
-       $("#about-text-9").removeClass("highlight", "about.css");
-       $("#about-text-10").removeClass("highlight", "about.css");
-       $("#about-text-11").removeClass("highlight", "about.css");
-       $("main").removeClass("about-bg-img-1", "about.css");
-       $("main").removeClass("about-bg-img-2", "about.css");
-       $("main").removeClass("about-bg-img-3", "about.css");
-       $("main").removeClass("about-bg-img-4", "about.css");
-       $("main").removeClass("about-bg-img-5", "about.css");
-       $("main").removeClass("about-bg-img-6", "about.css");
-       $("main").removeClass("about-bg-img-7", "about.css");
-       $("main").removeClass("about-bg-img-8", "about.css");
-       $("main").removeClass("about-bg-img-9", "about.css");
-       $("main").removeClass("about-bg-img-10", "about.css");
-       $("main").removeClass("about-bg-img-11", "about.css");
+    //    $("#about-text-1").removeClass("highlight", "about.css");
+    //    $("#about-text-2").removeClass("highlight", "about.css");
+    //    $("#about-text-3").removeClass("highlight", "about.css");
+    //    $("#about-text-4").removeClass("highlight", "about.css");
+    //    $("#about-text-5").removeClass("highlight", "about.css");
+    //    $("#about-text-6").removeClass("highlight", "about.css");
+    //    $("#about-text-7").removeClass("highlight", "about.css");
+    //    $("#about-text-8").removeClass("highlight", "about.css");
+    //    $("#about-text-9").removeClass("highlight", "about.css");
+    //    $("#about-text-10").removeClass("highlight", "about.css");
+    //    $("#about-text-11").removeClass("highlight", "about.css");
+       $("#about-text-1").removeClass("img-1", "about.css");
+       $("#about-text-2").removeClass("img-2", "about.css");
+       $("#about-text-3").removeClass("img-3", "about.css");
+       $("#about-text-4").removeClass("img-4", "about.css");
+       $("#about-text-5").removeClass("img-5", "about.css");
+       $("#about-text-6").removeClass("img-6", "about.css");
+    //    $("main").removeClass("about-bg-img-7", "about.css");
+    //    $("main").removeClass("about-bg-img-8", "about.css");
+    //    $("main").removeClass("about-bg-img-9", "about.css");
+    //    $("main").removeClass("about-bg-img-10", "about.css");
+    //    $("main").removeClass("about-bg-img-11", "about.css");
    }
 });
